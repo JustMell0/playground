0c1a9dd16b0667249a84ca6909af7da8904df4c2
JustMell0 <original.justmello1337@gmail.com>
2023-11-02 00:26:54 +0100

    This patch adds a new print function for formatted output. The print function takes a format string and variable arguments, supporting the formats %d, %f, and %s. This function can be useful for displaying formatted information, such as logging or debugging purposes.

---
+++ print/print.c
@@ -0,0 +1,49 @@
+#include "print.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+void print(const char *format, ...) {
+  va_list args;
+  va_start(args, format);
+
+  while (*format != '\0') {
+    if (*format == '%') {
+      format++;
+
+      switch (*format) {
+      case 'd': {
+        int temp = va_arg(args, int);
+        char buf[1000];
+        snprintf(buf, sizeof(buf), "%d", temp);
+        fprintf(stdout, buf);
+        break;
+      }
+      case 'f': {
+        double temp = va_arg(args, double);
+        char buf[1000];
+        snprintf(buf, sizeof(buf), "%f", temp);
+        fprintf(stdout, buf);
+        break;
+      }
+      case 's': {
+        const char *temp = va_arg(args, char *);
+        fprintf(stdout, temp);
+        break;
+      }
+      }
+    } else {
+      putchar(*format);
+    }
+    format++;
+  }
+  va_end(args);
+}
+
+int main() {
+  int lol = 10;
+  double kek = 10.23;
+  char *kek1 = "hello";
+
+  print("My vars: lol = %d, kek = %f, kek1=%s", lol, kek, kek1);
+  return 0;
+}
